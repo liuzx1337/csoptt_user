df43358d11df7c9d67d2eb03f3a4ee4d008e5810
刘子西 <Liuzx1337@163.com>
2018-11-28 15:19:36 +0800

    增加redis工具，作为bean注入到容器中

--- csoptt_user-main/src/main/java/com/csoptt/config/RedisConnectionConfig.java
+++ csoptt_user-main/src/main/java/com/csoptt/config/RedisConnectionConfig.java
@@ -1,5 +1,10 @@
 package com.csoptt.config;
 
+import com.csoptt.properties.RedisProperties;
+import com.csoptt.utils.jedis.JedisUtil;
+import com.csoptt.utils.jedis.RedisConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -10,4 +15,26 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class RedisConnectionConfig {
+
+    /**
+     * redis配置
+     */
+    @Autowired
+    private RedisProperties redisProperties;
+
+    /**
+     * redis工具
+     * @return
+     * @author qishao
+     * date 2018-11-28
+     */
+    @Bean
+    public JedisUtil jedisUtil() {
+        RedisConfig.redisHost = redisProperties.getHost();
+        RedisConfig.redisPort = redisProperties.getPort();
+        RedisConfig.redisPassword = redisProperties.getPassword();
+        RedisConfig.redisDbnum = redisProperties.getDatabase();
+
+        return JedisUtil.getInstance();
+    }
 }
